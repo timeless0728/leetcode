565a9c8994b0dd649e46946ec1a77feb0746d27d
timeless0728 <296560718@qq.com>
2016-06-25 22:12:05 +0800

    22 ==

---
+++ 22.py
@@ -0,0 +1,19 @@
+def getstr(left, right, str, res):
+    if not left and not right:
+        res.append(str)
+    if left > 0:
+        getstr(left - 1, right, str + '(', res)
+    if left < right:
+        getstr(left, right - 1, str + ')', res)
+    return res
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        return getstr(n,n,'',[])
+
+solution = Solution()
+print solution.generateParenthesis(3)
